ccc1a77845909f9c0deb307676f01ec3dfd3ca44
pnleguizamo <pnleguizamo@gmail.com>
2023-09-27 20:20:19 -0500

    scaleY

--- src/main/java/org/basics/TimeDomain.java
+++ src/main/java/org/basics/TimeDomain.java
@@ -33,15 +33,27 @@ public class TimeDomain implements GLEventListener {
         double maxRange = 2.0 / 5000;
         double tempX = -1;
         double tempY = yValues.getSample((yValues.getLength()) - (yValuesNum));
+        double max = tempY;
+        double min = tempY;
+        double scale;
 
         for (int i = 1; i<yValuesNum; i++){
             double x1 = -1 + i*maxRange;
             double y1 = yValues.getSample((yValues.getLength()) - (yValuesNum - (i)));
+            //System.out.println("y1 is: " + y1);
+            //System.out.println("y1 is: " + y1);
 
-            // double sample1x = -1.0 + ((i - 1) * xDiff);
+            min = Math.min(y1, min);
+            max = Math.max(y1, max);
+
+            double yRange = max - min;
+
+            // Formula for scaling y: (((y - min) / (range)) * actual range) + -1
+
+            double drawY1 = 0.0; // Scaled y1
+            double drawTempY = 0.0; // Scaled TempY
 
             gl.glBegin (GL2.GL_LINES);
-            //gl.glVertex2d(-0.80,0);
             gl.glVertex2d(tempX,tempY);
             gl.glVertex2d(x1, y1);
             gl.glEnd();
