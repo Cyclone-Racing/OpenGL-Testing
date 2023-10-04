61530bd15a6f752589632b38c43bb661a363863a
pnleguizamo <pnleguizamo@gmail.com>
2023-10-04 16:49:31 -0500

    pre-datasets

--- src/main/java/org/basics/TimeDomain.java
+++ src/main/java/org/basics/TimeDomain.java
@@ -33,27 +33,47 @@ public class TimeDomain implements GLEventListener {
         double maxRange = 2.0 / 5000;
         double tempX = -1;
         double tempY = yValues.getSample((yValues.getLength()) - (yValuesNum));
+        double drawTempY = tempY; // Scaled TempY
         double max = tempY;
         double min = tempY;
+        double drawY1 = 0.0; // Scaled y1
         double scale;
 
         for (int i = 1; i<yValuesNum; i++){
+            double y1 = yValues.getSample((yValues.getLength()) - (yValuesNum - (i)));
+            y1 = y1 *2;
+            min = Math.min(y1, min);
+//            System.out.println("max is: " + max);
+
+            max = Math.max(y1, max);
+//            System.out.println("min is: " + min);
+
+        }
+        double yRange = (max - min);
+        tempY = ((tempY-min)/(yRange)*2)-1;
+        for (int i = 1; i<yValuesNum; i++){
             double x1 = -1 + i*maxRange;
             double y1 = yValues.getSample((yValues.getLength()) - (yValuesNum - (i)));
-            //System.out.println("y1 is: " + y1);
-            //System.out.println("y1 is: " + y1);
+            y1 = y1 *2;
+//            System.out.println("y1 is: " + y1);
+//            System.out.println("tempy1 is: " + tempY);
 
-            min = Math.min(y1, min);
-            max = Math.max(y1, max);
 
-            double yRange = max - min;
+
+
+
+
 
             // Formula for scaling y: (((y - min) / (range)) * actual range) + -1
 
-            double drawY1 = 0.0; // Scaled y1
-            double drawTempY = 0.0; // Scaled TempY
+            // Drops scale to 0-1, then doubles value to put it on a 0-2 and takes 1 away to put it on -1 to 1
+            y1 = ((y1-min)/(yRange)*2)-1;
+
+            System.out.println("scaled y1 is: " + y1);
+
 
             gl.glBegin (GL2.GL_LINES);
+            //gl.glVertex2d(-0.80,0);
             gl.glVertex2d(tempX,tempY);
             gl.glVertex2d(x1, y1);
             gl.glEnd();
